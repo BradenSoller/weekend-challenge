08ac0b597d06e4437ccc5a33c960fb9adce04ad0
Braden Soller <braden.soller18@gmail.com>
2023-11-05 09:48:55 -0600

    trying to get final peices to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,91 @@
+//get calculations 
 console.log('client.js is sourced!');
+let result = 0;
+let clickedOperator;
+
+function getHistory() {
+    axios({
+        method: 'GET',
+        url: '/calculations'
+    }).then((response) => {
+        console.log('Confirmed server recieved form submission.')
+        let calculations = response.data
+        renderCalulation(calculations)
+
+    });
+}
+
+//when submit is pressed    
+function handleSubmit(event) {
+    event.preventDefault()
+    //gets inputs
+    let numOne = document.getElementById('numOne').value
+    let numTwo = document.getElementById('numTwo').value
+    // let operator = event
+
+    //clears inputs 
+    document.getElementById('numOne').value = "";
+    document.getElementById('numTwo').value = "";
+
+    let calculation = { numOne: numOne, numTwo: numTwo, operator: clickedOperator }
+
+  
+
+    // hey server, take this info pls
+    axios({
+        method: 'POST',
+        url: '/calculations',
+        data: calculation
+    })
+        .then((response) => {
+            console.log('Confirmed server recieved form submission.')
+            // call info retrieval 
+            getCalculations()
+            console.log('response: ', response);
+           renderCalulation(response.data)
+        }).catch((err) => {
+            console.error('error caught in axios call, err: ', err);
+        });
+}
+
+//making operator work
+function getOperator(event, operator) {
+    console.log('hit getOperator()');
+    console.log('event: ', event);
+    console.log('operator: ', operator);
+    event.preventDefault()
+    clickedOperator = operator;
+}
+//get back calculations from server
+function getCalculations() {
+    console.log('getCalculations() ran');
+    axios({
+        method: 'GET',
+        url: '/calculations'
+    }).then(function (response) {
+        console.log('GET response recieved from server! Response:', response);
+        let calculations = response.data;
+        console.log('Parsing data from response package. Items');
+        renderCalulation(calculations)
+    })
+}
+
+function renderCalulation(calculation) {
+    console.log('render');
+    let recentResult = document.getElementById('recentResult')
+    let resultHistory = document.getElementById('resultHistory')
+    recentResult.innerHTML = "";
+    resultHistory.innerHTML = "";
+    // const domElement = document.getElementById('resultHistory');
+    console.log('calculation: ', calculation);
+    recentResult.innerHTML =
+    `<p>${calculation[calculation.length - 1]} </p>`
+    // resultHistory += 
+
+    for (calc of calculation) {
+        let ptag = `<p> ${calc.numOne} ${calc.clickedOperator} ${calc.numTwo}= ${calc.result}</p>`
+        document.getElementById('resultHistory').innerHTML += ptag
+    }
+       
+    }
+    
--- server/server.js
+++ server/server.js
@@ -1,33 +1,71 @@
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.static('server/public'));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
+let calculations = [];
+  console.log(calculations)
+  
+// function orderOfOperations(cal) {
+//     if (cal.operator === "+") {
+//      result.push (cal.result = cal.numOne + cal.numTwo)
+//     } else if (cal.operator === "-") {
+//       cal.result = cal.numOne - cal.numTwo
+//     } else if (cal.operator === '/') {
+//       cal.result = cal.numOne / cal.numTwo
+//     } else if (cal.operator === '*') {
+//        cal.result = cal.numOne * cal.numTwo
+//     }
+// }
+function orderOfOperations(numOne, numTwo, operation) {
+  console.log('ran orderOfOperations()');
+  console.log('numOne: ', numOne);
+  console.log('numTwo: ', numTwo);
+  console.log('operation: ', operation);
 
+  if (operation === "/")
+    return parseInt(numOne) / parseInt(numTwo)
+  else if (operation === '+')
+    return parseInt(numOne) + parseInt(numTwo)
+  else if (operation === "-")
+    return parseInt(numOne) - parseInt(numTwo)
+  else if (operation === "*")
+    return parseInt(numOne) * parseInt(numTwo)
+}
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
 app.get('/calculations', (req, res) => {
-  console.log('POST request received!');
-})
+  console.log('GET request received!',calculations);
+  console.log('req.body: ', req.body);
+  res.send(calculations);
+});
 // POST /calculations
 app.post('/calculations', (req, res) => {
   console.log('POST request received!');
-  console.log('POST request received!');
+  console.log('req.body in POST, req.body:', req.body);
+  let newCalculations = req.body;
+  orderOfOperations(newCalculations)
+  // let calculated = orderOfOperations(req.body.numOne, req.body.numTwo, req.body.operator);
+  // console.log('calculated: ', calculated);
+  // calculations.push(calculated)
+  // res.sendStatus(201)
+  // res.send(calculations)
+  res.sendStatus(201)
 })
+
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
 // Makes it so you don't have to kill the server
 // on 5000 in order to run the tests:
 if (process.env.NODE_ENV === 'test') {
-  PORT = 5001;
+  PORT = 5000;
 }
 
 // This starts the server...but also stores it in a variable.
